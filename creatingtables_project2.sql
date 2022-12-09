5e6201dc64fe4b2e41f4be59a612ccb6328d0f25
Nivedha Raja <107441044+rraja30@users.noreply.github.com>
2022-12-08 21:52:44 -0500

    Add files via upload

--- creatingtables_project2.sql
+++ creatingtables_project2.sql
@@ -2,15 +2,21 @@ DROP TABLE infrastructure;
 DROP TABLE accidents; 
 
 create table infrastructure(
-	OBJECTID int PRIMARY key NOT NULL, 
-	STREETNAME  varchar(50) NOT NULL, 
-	TYPE varchar(40) NOT NULL
+	objectid int PRIMARY key NOT NULL, 
+	street_name_lane  varchar(50) NOT NULL, 
+	lane_type varchar(40) NOT NULL
 )
 
 create table accidents(
 	id serial,
-	STREET_NAME varchar(50) NOT NULL, 
-	BICYCLISTS INT NOT NULL, 
-	INJURIES INT NOT NULL,
-	MAJOR_INJURIES INT NOT NULL
+	street_name varchar(100) NOT NULL, 
+	bicyclists INT NOT NULL, 
+	injuries INT NOT NULL,
+	major_injuries INT NOT NULL, 
+	street_name_clean varchar(100) NOT NULL
 )
+
+select accidents.street_name_clean,infrastructure.objectid,infrastructure.street_name_lane, infrastructure.lane_type, accidents.id, accidents.bicyclists, accidents.injuries, accidents.major_injuries, accidents.street_name
+from accidents
+inner join infrastructure
+on infrastructure.street_name_lane = accidents.street_name_clean;
